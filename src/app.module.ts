88d41379db380ec82a6da50cc297ebeb2cc745c7
One-Zee <harunadindane15@gmail.com>
2024-10-24 13:52:45 +0200

    swagger,rate-limit,cors,helmet,logger,excpetions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { rateLimitoptions } from './common/config';
+import { HttpLoggerMiddleware } from './middleware/logging/logging.middleware';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ThrottlerModule.forRoot([rateLimitoptions]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(HttpLoggerMiddleware).forRoutes('*');
+  }
+}
