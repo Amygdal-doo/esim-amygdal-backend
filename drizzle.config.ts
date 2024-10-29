a67a289ff80b94f273ab622d8d2ea8a75dee9679
One-Zee <harunadindane15@gmail.com>
2024-10-29 10:01:10 +0100

    updated drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'drizzle-kit';
 import 'dotenv/config';
 
 export default defineConfig({
-  schema: './src/modules/drizzle/schemas/**.schema.ts',
-  //   out: './src/modules/drizzle/drizzle.ts',
+  schema: './src/modules/**/schema.ts',
+  out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL,
